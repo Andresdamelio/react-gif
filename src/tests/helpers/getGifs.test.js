f2d85011846e3a6062eff2aabf220c53e200bce2
Andres D'amelio <andresdamelio24@gmail.com>
2022-03-18 11:29:36 -0400

    Test function fetch

---
+++ src/tests/helpers/getGifs.test.js
@@ -0,0 +1,13 @@
+import { getGifs } from '../../helpers/getGifs';
+
+describe('Pruebas en helper getGifs Fetch', () => {
+  test('Debe de traer 10 elementos', async () => {
+    const gifs = await getGifs('One Punch');
+    expect(gifs.length).toBe(10);
+  });
+
+  test('Debe de traer un arreglo vacio si no se envía la categoría', async () => {
+    const gifs = await getGifs('');
+    expect(gifs.length).toBe(0);
+  });
+});
